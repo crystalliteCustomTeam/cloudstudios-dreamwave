48127bb45bf513fcc1f6b33706f469b75eedd5fb
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2024-08-08 22:42:13 +0500

    header Changes

--- src/app/layout/header.jsx
+++ src/app/layout/header.jsx
@@ -1,14 +1,26 @@
 
+"use client"
 import { MainLogo } from "@/src/app-constants";
 import styles from "@/styles/layout/header.module.scss";
 import Link from "next/link";
 import { Col, Container, Row } from "react-bootstrap";
 import Navigation from "@/src/app/layout/navigation";
+import { useState, useEffect } from "react";
 
 const Header = () => {
-
+    const [scrolled, setScrolled] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 10;
+            setScrolled(isScrolled);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
-        <section className={styles.headerSec}>
+        <section className={`${styles.headerSec} ${scrolled ? styles.active : ''}`}>
             <Container className="h-100">
                 <Row className="h-100">
                     <Col xxl={2} xl={2} lg={2} md={2} sm={2} xs={6} className="my-auto">
