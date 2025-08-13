6469d9f47b38e26db3f0eca8179b8499b383a50a
Harry Kennedy <72253270+Harrythedevelopercs@users.noreply.github.com>
2025-08-13 05:48:56 +0500

    chat added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "../../styles/globals.scss"
 import ScriptsCode from "@/src/app/scripts"
 import ConditionalLayout from "./conditionallayout"
-
+import Script from "next/script"
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
